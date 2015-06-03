80f8d3b2eb5ca180b49525cfb395725a71e4111a
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-03 04:18:52 -0400

    Retrieve login from json

--- test/models/repo_test.rb
+++ test/models/repo_test.rb
@@ -9,10 +9,14 @@ end
 
 class RepoTest < ActiveSupport::TestCase
   test "repository name" do
-    assert_equal 11, Repo.new("jenbeckham").repository_name.count
+    assert_equal "battleship", Repo.new("jenbeckham").repository_name
   end
 
   test "picture url" do
     assert_equal "https://avatars.githubusercontent.com/u/11805662?v=3", Repo.new("jenbeckham").picture
+  end
+
+  test "login" do
+    assert_equal "jenbeckham", Repo.new("jenbeckham").login
   end
 end
